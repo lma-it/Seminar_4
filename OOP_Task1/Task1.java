164b037a0c717300c4b91efdca3a4bed1aeb3228
Михаил Лубенцов <wispergently061@gmail.com>
2024-07-03 18:37:12 +0500

    Настроена среда разработки для дальнейшей работы.

---
+++ OOP_Task1/Task1.java
@@ -0,0 +1,12 @@
+package OOP_Task1;
+import static print_module.print_library.println;
+
+/**
+ * Task1
+ */
+public class Task1 {
+
+    public static void main(String[] args) {
+        println("Hello");
+    }
+}
